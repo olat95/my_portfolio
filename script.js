a6f735f8fb74825d296fbe5ff1a248c0c21a0bdb
Olatunji Oluwapelumi Samson <samsontunj@gmail.com>
2021-08-20 21:01:19 +0100

    changing color and email

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log('cool')
+
+let theme = localStorage.getItem('theme')
+if (theme == null){
+  setTheme('orange')
+} else {
+  setTheme(theme)
+}
+
+let themeBoxes = document.getElementsByClassName('theme-box')
+
+for (let i = 0; themeBoxes.length > i; i++) {
+  themeBoxes[i].addEventListener('click', function(){
+    let mode = this.dataset.mode
+    console.log('box clicked: ', mode)
+    setTheme(mode)
+  })
+}
+
+function setTheme(mode) {
+  if(mode == 'orange'){
+    document.getElementById('theme-color').href = 'css/style.css'
+  }
+  if(mode == 'dark'){
+    document.getElementById('theme-color').href = 'css/black.css'
+  }
+  if(mode == 'sky'){
+    document.getElementById('theme-color').href = 'css/blue.css'
+  }
+  if(mode == 'light'){
+    document.getElementById('theme-color').href = 'css/light.css'
+  }
+
+  localStorage.setItem('theme', mode)
+}
